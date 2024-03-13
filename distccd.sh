547e6e25de95086f17f1b8e59f3caf25abdda503
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-03-13 17:07:34 +0900

    Update distccd.sh

--- distccd.sh
+++ distccd.sh
@@ -4,7 +4,7 @@ set -x
 
 export PS4='+(${BASH_SOURCE}:${DISTCCD_HOST}:${LINENO}): '
 
-curl -sSm 3 https://${DISTCCD_HOST}/ >/dev/null 2>&1 &
+curl -sSm 3 https://"${DISTCCD_HOST}"/ >/dev/null 2>&1 &
 
 PASSWORD="$(echo -n "${DISTCCD_HOST}""${DUMMY_STRING_1}""$(date +%Y/%m/%d)" | base64 -w 0 | sed 's/[+\/=]//g')"
 
@@ -16,11 +16,11 @@ while [ "200" != "$(curl -sSu ${BASIC_USER}:${BASIC_PASSWORD} -o /dev/null -w '%
   sleep 3s
 done
 
-KEYWORD=$(curl -sSu ${BASIC_USER}:${BASIC_PASSWORD} https://${DISTCCD_HOST}/auth/${KEYWORD_FILENAME})
-SSH_USER=$(curl -sSu ${BASIC_USER}:${BASIC_PASSWORD} https://${DISTCCD_HOST}/auth/${SSH_USER_FILENAME})
+KEYWORD="$(curl -sSu ${BASIC_USER}:${BASIC_PASSWORD} https://${DISTCCD_HOST}/auth/${KEYWORD_FILENAME})"
+SSH_USER="$(curl -sSu ${BASIC_USER}:${BASIC_PASSWORD} https://${DISTCCD_HOST}/auth/${SSH_USER_FILENAME})"
 
-curl -sSu ${BASIC_USER}:${BASIC_PASSWORD} -O https://${DISTCCD_HOST}/auth/${SSH_KEY_FILENAME}
-chmod 600 ./${SSH_KEY_FILENAME}
+curl -sSu "${BASIC_USER}":"${BASIC_PASSWORD}" -O https://"${DISTCCD_HOST}"/auth/"${SSH_KEY_FILENAME}"
+chmod 600 ./"${SSH_KEY_FILENAME}"
 
 CURL_OPT="${CURL_OPT} -m 3600 -sSN"
 
@@ -29,13 +29,12 @@ CURL_OPT="${CURL_OPT} -m 3600 -sSN"
 #   | nc -lp ${SSH_PORT} -s 127.0.0.1 \
 #   | stdbuf -i0 -o0 openssl aes-128-ctr -pass "pass:${PASSWORD}" -bufsize 1 -pbkdf2 -iter 1 -md md5 \
 #   | curl ${CURL_OPT} -T - ${PIPING_SERVER}/${KEYWORD}req &
-curl ${CURL_OPT} ${PIPING_SERVER}/${KEYWORD}res \
-  | nc -lp ${SSH_PORT} -s 127.0.0.1 \
-  | curl ${CURL_OPT} -T - ${PIPING_SERVER}/${KEYWORD}req &
+curl ${CURL_OPT} "${PIPING_SERVER}"/"${KEYWORD}"res \
+  | nc -lp "${SSH_PORT}" -s 127.0.0.1 \
+  | curl ${CURL_OPT} -T - "${PIPING_SERVER}"/"${KEYWORD}"req &
 
 ssh -v -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
   -o ServerAliveInterval=60 -o ServerAliveCountMax=60 \
-  -p ${SSH_PORT} \
-  -i ./${SSH_KEY_FILENAME} \
-  -4fNL ${DISTCC_PORT}:127.0.0.1:3632 ${SSH_USER}@127.0.0.1 &
-
+  -p "${SSH_PORT}" \
+  -i ./"${SSH_KEY_FILENAME}" \
+  -4fNL "${DISTCC_PORT}":127.0.0.1:3632 "${SSH_USER}"@127.0.0.1 &
