ef37e88da3a5e1e64080049d06011987eec4891f
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2024-03-15 14:54:39 +0900

    Update distccd.sh

--- distccd.sh
+++ distccd.sh
@@ -49,7 +49,7 @@ for ((i=0; i < 5; i++)); do \
 
   sleep 2s
 
-  ss -antp | grep ESTAB | grep \:${SSH_PORT} | grep -o pid=(\d+), | grep -v grep
+  ss -antp | grep ESTAB | grep \:${SSH_PORT} | grep -o "pid=(\d+)," | grep -v grep
 
   if [ $(grep -c -i error /tmp/ssh_${CONNECT_PORT}.log) -eq 0 ]; then
     break
